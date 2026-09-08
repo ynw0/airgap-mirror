c3f93dd05ab04247fe31d7e552938def16ab6c18
ynw <104645375+ynw0@users.noreply.github.com>
2026-09-08 14:18:49 +0800

    refactor: reuse default adapter registry

--- cmd/mirror-agent/main.go
+++ cmd/mirror-agent/main.go
@@ -15,10 +15,6 @@ import (
 
 	_ "modernc.org/sqlite"
 
-	"github.com/ynw0/airgap-mirror/internal/adapters/apt"
-	"github.com/ynw0/airgap-mirror/internal/adapters/maven"
-	"github.com/ynw0/airgap-mirror/internal/adapters/npm"
-	"github.com/ynw0/airgap-mirror/internal/adapters/pypi"
 	appserver "github.com/ynw0/airgap-mirror/internal/server"
 	"github.com/ynw0/airgap-mirror/internal/service"
 	storesqlite "github.com/ynw0/airgap-mirror/internal/storage/sqlite"
@@ -54,13 +50,7 @@ func main() {
 	}
 	cancel()
 	store := storesqlite.NewServerStore(db)
-	registry, err := service.NewRegistry(
-		apt.New(http.DefaultClient),
-		pypi.New(http.DefaultClient),
-		npm.New(http.DefaultClient),
-		maven.NewGeneric(http.DefaultClient),
-		maven.NewCentral(http.DefaultClient),
-	)
+	registry, err := service.NewDefaultRegistry(http.DefaultClient)
 	if err != nil {
 		log.Fatal(err)
 	}
