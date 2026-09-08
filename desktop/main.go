bb2d65fad5385a7f35869a9d615c7b5d271d3d97
ynw <104645375+ynw0@users.noreply.github.com>
2026-09-08 14:38:49 +0800

    feat: add Wails desktop entrypoint

---
+++ desktop/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"embed"
+	"log"
+
+	"github.com/wailsapp/wails/v2"
+	"github.com/wailsapp/wails/v2/pkg/options"
+	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
+)
+
+//go:embed all:frontend/dist
+var assets embed.FS
+
+func main() {
+	app := NewApp()
+	if err := wails.Run(&options.App{
+		Title:     "Airgap Mirror",
+		Width:     1360,
+		Height:    860,
+		MinWidth:  1120,
+		MinHeight: 720,
+		AssetServer: &assetserver.Options{
+			Assets: assets,
+		},
+		OnStartup:  app.startup,
+		OnShutdown: app.shutdown,
+		Bind: []interface{}{
+			app,
+		},
+	}); err != nil {
+		log.Fatal(err)
+	}
+}
